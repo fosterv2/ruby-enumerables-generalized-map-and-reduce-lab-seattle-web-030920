d1870ca463e1cfb29a093dab970abb012023a409
Valerie Foster <fosterv2@outlook.com>
2020-01-22 00:32:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    new_array << yield(source_array[index])
+    index += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  result = starting_point
+  index = 0
+  while index < source_array.length do
+    result = 0
+    index += 1
+  end
+  result
+end
